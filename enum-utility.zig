020ab416c7e89f99500664825270e560c803e4fc
InKryption <inkryption07@gmail.com>
2022-03-11 19:30:10 +0100

    Fixed the zero-bit enum issue, and did some re-organizing

--- enum-utility.zig
+++ enum-utility.zig
@@ -6,36 +6,48 @@ pub fn combinedEnumsFieldCount(comptime A: type, comptime B: type) comptime_int 
 }
 
 pub fn CombinedEnums(comptime A: type, comptime B: type) type {
-    comptime {
-        const name_separator = "_";
-        var new_fields: []const std.builtin.TypeInfo.EnumField = &[_]std.builtin.TypeInfo.EnumField{};
+    const name_separator = "_".*;
+    var new_fields: []const std.builtin.TypeInfo.EnumField = &[_]std.builtin.TypeInfo.EnumField{};
 
-        for (@typeInfo(A).Enum.fields) |field_a| {
-            const a = @field(A, field_a.name);
-            for (@typeInfo(B).Enum.fields) |field_b| {
-                const b = @field(B, field_b.name);
+    const TagNamespace = CombinedEnumsTagNamespace(A, B);
 
-                const tag = if (A == u0 and B == u0) 0 else combinedEnumsTag(a, b);
-                new_fields = new_fields ++ [_]std.builtin.TypeInfo.EnumField{.{
-                    .name = @tagName(a) ++ name_separator ++ @tagName(b),
-                    .value = tag,
-                }};
-            }
+    std.debug.assert(TagNamespace.info_a.is_exhaustive);
+    std.debug.assert(TagNamespace.info_b.is_exhaustive);
+
+    for (TagNamespace.info_a.fields) |field_a| {
+        const a = @field(A, field_a.name);
+        for (TagNamespace.info_b.fields) |field_b| {
+            const b = @field(B, field_b.name);
+
+            const tag = TagNamespace.tagCombined(a, b);
+            new_fields = new_fields ++ [_]std.builtin.TypeInfo.EnumField{.{
+                .name = @tagName(a) ++ name_separator ++ @tagName(b),
+                .value = tag,
+            }};
         }
-
-        return @Type(@unionInit(std.builtin.TypeInfo, "Enum", std.builtin.TypeInfo.Enum{
-            .layout = .Auto,
-            .tag_type = CombinedEnumsTag(A, B),
-            .fields = new_fields,
-            .decls = &[_]std.builtin.TypeInfo.Declaration{},
-            .is_exhaustive = true,
-        }));
     }
+
+    return @Type(@unionInit(std.builtin.TypeInfo, "Enum", std.builtin.TypeInfo.Enum{
+        .layout = .Auto,
+        .tag_type = TagNamespace.TagCombined,
+        .fields = new_fields,
+        .decls = &[_]std.builtin.TypeInfo.Declaration{},
+        .is_exhaustive = true,
+    }));
 }
 
-pub fn combineEnums(a: anytype, b: anytype) CombinedEnums(@TypeOf(a), @TypeOf(b)) {
-    const Combined = CombinedEnums(@TypeOf(a), @TypeOf(b));
-    return @intToEnum(Combined, combinedEnumsTag(a, b));
+pub noinline fn combineEnums(a: anytype, b: anytype) CombinedEnums(@TypeOf(a), @TypeOf(b)) {
+    const A = @TypeOf(a);
+    const B = @TypeOf(b);
+    const TagNamespace = CombinedEnumsTagNamespace(A, B);
+
+    // TODO: Remove this once casting to `u0` works.
+    // This is a work around for casting 0 to `u0` crashing the compiler
+    comptime if (TagNamespace.TagCombined == u0) {
+        return std.enums.values(CombinedEnums(A, B))[0];
+    };
+
+    return @intToEnum(CombinedEnums(A, B), TagNamespace.tagCombined(a, b));
 }
 
 pub fn combineEnumsTemplate(comptime A: type, comptime B: type) (fn (A, B) CombinedEnums(A, B)) {
@@ -46,102 +58,98 @@ pub fn combineEnumsTemplate(comptime A: type, comptime B: type) (fn (A, B) Combi
     }.combineEnumsFn;
 }
 
-fn CombinedEnumsTag(comptime A: type, comptime B: type) type {
-    const TagA = @typeInfo(A).Enum.tag_type;
-    const bits_a = @typeInfo(TagA).Int.bits;
+fn CombinedEnumsTagNamespace(comptime A: type, comptime B: type) type {
+    return struct {
+        const info_a: std.builtin.TypeInfo.Enum = @typeInfo(A).Enum;
+        const info_b: std.builtin.TypeInfo.Enum = @typeInfo(B).Enum;
 
-    const TagB = @typeInfo(B).Enum.tag_type;
-    const bits_b = @typeInfo(TagB).Int.bits;
+        const TagA = info_a.tag_type;
+        const TagB = info_b.tag_type;
 
-    return std.meta.Int(.unsigned, bits_a + bits_b);
-}
+        const bits_a = @typeInfo(TagA).Int.bits;
+        const bits_b = @typeInfo(TagB).Int.bits;
 
-inline fn combinedEnumsTag(a: anytype, b: anytype) CombinedEnumsTag(@TypeOf(a), @TypeOf(b)) {
-    const A = @TypeOf(a);
-    const B = @TypeOf(b);
-    const Combined = CombinedEnumsTag(A, B);
+        const UnsignedA = std.meta.Int(.unsigned, bits_a);
+        const UnsignedB = std.meta.Int(.unsigned, bits_b);
 
-    const TagA = @typeInfo(A).Enum.tag_type;
-    const TagB = @typeInfo(B).Enum.tag_type;
+        const TagCombined = std.meta.Int(.unsigned, bits_a + bits_b);
 
-    const bits_a = @typeInfo(TagA).Int.bits;
-    const bits_b = @typeInfo(TagB).Int.bits;
+        const values_a: *const [info_a.fields.len]A = std.enums.values(A)[0..info_a.fields.len];
+        const values_b: *const [info_b.fields.len]B = std.enums.values(B)[0..info_b.fields.len];
 
-    const UnsignedA = std.meta.Int(.unsigned, bits_a);
-    const UnsignedB = std.meta.Int(.unsigned, bits_b);
+        const asc_values_a: *const [info_a.fields.len]A = asc_values_a: {
+            var asc_values: [info_a.fields.len]A = values_a.*;
+            @setEvalBranchQuota(std.math.min(std.math.maxInt(u32), 1000 + asc_values.len));
 
-    if (comptime bits_a == 0 and bits_b == 0) {
-        switch (comptime builtin.zig_backend) {
-            .stage1 => @compileError(
-                \\Note: A compiler bug makes it impossible to support combining two enums which both have 0 bit
-                \\backing integers, so we'll have to wait until stage2 to see if its fixed.
-            ),
-            .stage2_llvm,
-            .stage2_c,
-            .stage2_wasm,
-            .stage2_arm,
-            .stage2_x86_64,
-            .stage2_aarch64,
-            .stage2_x86,
-            .stage2_riscv64,
-            => @compileError(
-                \\Either remove this code, or change this message to indicate the code still doesn't work
-                \\for the given backend.
-            ),
-            .other => {},
-            _ => {},
+            std.sort.sort(A, &asc_values, void{}, struct {
+                fn lessThan(context: void, lhs: A, rhs: A) bool {
+                    _ = context;
+                    return @enumToInt(lhs) < @enumToInt(rhs);
+                }
+            }.lessThan);
+
+            break :asc_values_a &asc_values;
+        };
+        const asc_values_b: *const [info_b.fields.len]B = asc_values_b: {
+            var asc_values: [info_b.fields.len]B = values_b.*;
+            @setEvalBranchQuota(std.math.min(std.math.maxInt(u32), 1000 + asc_values.len));
+
+            std.sort.sort(B, &asc_values, void{}, struct {
+                fn lessThan(context: void, lhs: B, rhs: B) bool {
+                    _ = context;
+                    return @enumToInt(lhs) < @enumToInt(rhs);
+                }
+            }.lessThan);
+
+            break :asc_values_b &asc_values;
+        };
+
+        const min_a = asc_values_a[0];
+        const min_b = asc_values_b[0];
+
+        const max_a = asc_values_a[asc_values_a.len - 1];
+        const max_b = asc_values_b[asc_values_a.len - 1];
+
+        fn assertValidCombo() void {
+            if (comptime values_a.len == 0 or values_b.len == 0) {
+                @compileError("Cannot combine enums with zero members.");
+            }
         }
-        return 0;
-    }
 
-    if (comptime bits_a == 0 and bits_b != 0) return @bitCast(UnsignedB, @enumToInt(b));
-    if (comptime bits_a != 0 and bits_b == 0) return @bitCast(UnsignedA, @enumToInt(a));
-
-    const a_min = comptime @enumToInt(enumMinMax(A).min);
-    const b_min = comptime @enumToInt(enumMinMax(B).min);
-
-    const tag_a: Combined = @bitCast(UnsignedA, @enumToInt(a));
-    const tag_b: Combined = @bitCast(UnsignedB, @enumToInt(b));
-
-    return ((tag_a - a_min) << bits_b) | (tag_b - b_min);
-}
-
-inline fn enumMinMax(comptime Enum: type) struct { min: Enum, max: Enum } {
-    const lessThan = comptime struct {
-        fn lessThan(context: void, lhs: Enum, rhs: Enum) bool {
-            _ = context;
-            return std.math.order(@enumToInt(lhs), @enumToInt(rhs)) == .lt;
+        fn unsignedA(a: A) UnsignedA {
+            assertValidCombo();
+            return @bitCast(UnsignedA, @enumToInt(a));
         }
-    }.lessThan;
-    const values = comptime std.enums.values(Enum);
-    return .{
-        .min = values[std.sort.argMin(Enum, values, void{}, lessThan).?],
-        .max = values[std.sort.argMax(Enum, values, void{}, lessThan).?],
+
+        fn unsignedB(b: B) UnsignedB {
+            assertValidCombo();
+            return @bitCast(UnsignedB, @enumToInt(b));
+        }
+
+        const Bits = packed struct { a: TagA, b: TagB };
+
+        fn tagCombined(a: A, b: B) TagCombined {
+            assertValidCombo();
+            return @bitCast(TagCombined, Bits{
+                .a = @enumToInt(a) - @enumToInt(min_a),
+                .b = @enumToInt(b) - @enumToInt(min_b),
+            });
+        }
+
+        fn extractTagA(combined: TagCombined) TagA {
+            assertValidCombo();
+            return @bitCast(Bits, combined).a + @enumToInt(min_a);
+        }
+
+        fn extractTagB(combined: TagCombined) TagB {
+            assertValidCombo();
+            return @bitCast(Bits, combined).b + @enumToInt(min_b);
+        }
     };
 }
 
-inline fn combinedEnumsATag(comptime A: type, comptime B: type, combined: CombinedEnums(A, B)) std.meta.Tag(A) {
-    const TagA = @typeInfo(A).Enum.tag_type;
-    const TagB = @typeInfo(B).Enum.tag_type;
-
-    const bits_b = @typeInfo(TagB).Int.bits;
-    const a_min = comptime @enumToInt(enumMinMax(A).min);
-
-    const tag = @enumToInt(combined);
-    return @truncate(TagA, (tag >> bits_b) + a_min);
-}
-
-inline fn combinedEnumsBTag(comptime A: type, comptime B: type, combined: CombinedEnums(A, B)) std.meta.Tag(B) {
-    const TagB = @typeInfo(B).Enum.tag_type;
-
-    const b_min = comptime @enumToInt(enumMinMax(B).min);
-
-    const tag = @enumToInt(combined);
-    return @truncate(TagB, tag) + b_min;
-}
-
 pub const SplitCombinedEnumComponents = enum { a, b, both };
-pub fn splitCombinedEnumTemplate(
+pub fn splitCombinedEnumsTemplate(
     comptime A: type,
     comptime B: type,
     comptime split: SplitCombinedEnumComponents,
@@ -157,47 +165,69 @@ pub fn splitCombinedEnumTemplate(
         .both => std.meta.ArgsTuple(@TypeOf(combineEnumsTemplate(A, B))),
     };
 
+    const TagNamespace = CombinedEnumsTagNamespace(A, B);
+
     return comptime struct {
         fn splitCombinedEnum(combined: Combined) ReturnType {
-            switch (comptime split) {
-                .a => return @intToEnum(A, combinedEnumsATag(A, B, combined)),
-                .b => return @intToEnum(B, combinedEnumsBTag(A, B, combined)),
-                .both => return .{
-                    @intToEnum(A, combinedEnumsATag(A, B, combined)),
-                    @intToEnum(B, combinedEnumsBTag(A, B, combined)),
+            // TODO: Remove this once casting to `u0` works.
+            // This is a work around for casting 0 to `u0` crashing the compiler
+            comptime if (TagNamespace.TagCombined == u0) return switch (split) {
+                .a, .b => std.enums.values(ReturnType)[0],
+                .both => .{
+                    .a = std.enums.values(A)[0],
+                    .b = return std.enums.values(B)[0],
                 },
-            }
+            };
+
+            return switch (comptime split) {
+                .a => @intToEnum(A, CombinedEnumsTagNamespace(A, B).extractTagA(@enumToInt(combined))),
+                .b => @intToEnum(B, CombinedEnumsTagNamespace(A, B).extractTagB(@enumToInt(combined))),
+                .both => .{
+                    @intToEnum(A, CombinedEnumsTagNamespace(A, B).extractTagA(@enumToInt(combined))),
+                    @intToEnum(B, CombinedEnumsTagNamespace(A, B).extractTagB(@enumToInt(combined))),
+                },
+            };
         }
     }.splitCombinedEnum;
 }
 
 test "combineEnums" {
-    const Direction = enum(u8) { north = 255, east = 33, south = 29, west = 11 };
-    const Rotation = enum(u8) { clockwise, anticlockwise };
-    const Combined = CombinedEnums(Direction, Rotation);
-
-    const combineDirRot = comptime combineEnumsTemplate(Direction, Rotation);
-
-    const getBoth = comptime splitCombinedEnumTemplate(Direction, Rotation, .both);
-    const getDir = comptime splitCombinedEnumTemplate(Direction, Rotation, .a);
-    const getRot = comptime splitCombinedEnumTemplate(Direction, Rotation, .b);
-
-    for (std.enums.values(Direction)) |expected_d| {
-        for (std.enums.values(Rotation)) |expected_r| {
-            const combined = combineDirRot(expected_d, expected_r);
-            const actual_d = getDir(combined);
-            const actual_r = getRot(combined);
-
-            try std.testing.expectEqual(expected_r, actual_r);
-            try std.testing.expectEqual(expected_d, actual_d);
-
-            try std.testing.expectEqual(combineDirRot(expected_d, expected_r), combined);
-            try std.testing.expectEqual(combined, @call(.{}, combineDirRot, getBoth(combined)));
-        }
+    {
+        const Foo = enum { foo };
+        const Bar = enum { bar };
+        try std.testing.expectEqual(CombinedEnums(Foo, Bar).foo_bar, combineEnums(Foo.foo, Bar.bar));
+        try std.testing.expectEqual(Foo.foo, splitCombinedEnumsTemplate(Foo, Bar, .a)(combineEnums(Foo.foo, Bar.bar)));
+        try std.testing.expectEqual(Bar.bar, splitCombinedEnumsTemplate(Foo, Bar, .b)(combineEnums(Foo.foo, Bar.bar)));
     }
 
-    comptime try std.testing.expect(@hasField(Combined, "north_clockwise"));
-    try std.testing.expectEqual(Combined.north_clockwise, combineDirRot(.north, .clockwise));
+    {
+        const Direction = enum(u8) { north, east, south, west };
+        const Rotation = enum(u8) { clockwise, anticlockwise };
+        const Combined = CombinedEnums(Direction, Rotation);
+
+        const combineDirRot = comptime combineEnumsTemplate(Direction, Rotation);
+
+        const getBoth = comptime splitCombinedEnumsTemplate(Direction, Rotation, .both);
+        const getDir = comptime splitCombinedEnumsTemplate(Direction, Rotation, .a);
+        const getRot = comptime splitCombinedEnumsTemplate(Direction, Rotation, .b);
+
+        for (std.enums.values(Direction)) |expected_d| {
+            for (std.enums.values(Rotation)) |expected_r| {
+                const combined = combineDirRot(expected_d, expected_r);
+                const actual_d = getDir(combined);
+                const actual_r = getRot(combined);
+
+                try std.testing.expectEqual(expected_r, actual_r);
+                try std.testing.expectEqual(expected_d, actual_d);
+
+                try std.testing.expectEqual(combineDirRot(expected_d, expected_r), combined);
+                try std.testing.expectEqual(combined, @call(.{}, combineDirRot, getBoth(combined)));
+            }
+        }
+
+        comptime try std.testing.expect(@hasField(Combined, "north_clockwise"));
+        try std.testing.expectEqual(Combined.north_clockwise, combineDirRot(.north, .clockwise));
+    }
 }
 
 /// Attempts to cast an enum to another enum, by name. Returns null
